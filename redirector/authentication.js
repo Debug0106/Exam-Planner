5d0d76e587de369314edb7211819a5ec5ea834d2
Debug0106 <90171993+Debug0106@users.noreply.github.com>
2021-09-06 10:46:59 +0530

    Project upload

---
+++ redirector/authentication.js
@@ -0,0 +1,21 @@
+var animationStarted = false;
+
+/* We could also have used auth.onStateChanged, but that doesn't get called in disconnections
+*/
+
+checkConnectionEvery2Seconds();
+
+function checkConnectionEvery2Seconds() {
+    setTimeout(function () {
+        checkConnectionEvery2Seconds();
+    }, 1000);
+
+    var connectedRef = database.ref(".info/connected");
+    connectedRef.on("value", function (snap) {
+        console.log(snap);
+        if (snap.val()) {
+            if (auth.currentUser) location.href = "https://peeyush0106.github.io/Silicon-Valley-Challengesigned-in/index.html";
+            else location.href = "https://peeyush0106.github.io/Silicon-Valley-Challenge/signin/index.html";
+        }
+    });
+}
